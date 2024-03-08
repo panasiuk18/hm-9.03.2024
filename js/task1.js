1c1db5648f4e38dadfef75e6556c752390073aa1
panasiuk18 <156125172+panasiuk18@users.noreply.github.com>
2024-03-08 16:29:21 +0200

    Add files via upload

---
+++ js/task1.js
@@ -0,0 +1,35 @@
+//* Створіть функцію personInfo, яка приймає об'єкт в якості параметра та виконує деструктуризацію цього об'єкта, витягуючи з нього наступні поля: "name", "age", "email", "city". Потім використайте деструктуризовані значення для створення нового об'єкта з наступними полями: "fullName" (значення поля "name" додане до значення поля "age" через пробіл), "contact" (значення поля "email" додане до значення поля "city" через кому). Функція має повертати цей новий об'єкт.
+
+// // * Маємо наступний об'єкт:
+// const person = {
+//     name: 'John Doe',
+//     age: 30,
+//     email: 'john.doe@example.com',
+//     city: 'New York',
+//   };
+// 
+//   console.log(personInfo(person)); //
+//   //* Маєте отримати результат:
+//   // contact: "john.doe@example.com, New York"
+//   // fullName: "John Doe, 30"
+
+function personInfo(obj) {
+
+  const { name, age, email, city } = obj;
+
+  const personInfo = {
+    fullName: `${name}, ${age}`,
+    contact: `${email}, ${city}`,
+  };
+
+  return personInfo;
+}
+const person = {
+  name: 'John Doe',
+  age: 30,
+  email: 'john.doe@example.com',
+  city: 'New York',
+};
+
+const personInfoObj = personInfo(person);
+console.log(personInfoObj);
